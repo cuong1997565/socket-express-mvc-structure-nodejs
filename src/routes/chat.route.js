0beccc80c2269538a0402cd9bf81a13a5ca182ff
cuongnx <cuong1997565@gmail.com>
2023-12-16 09:32:53 +0700

    first

--- src/routes/chat.route.js
+++ src/routes/chat.route.js
@@ -2,6 +2,13 @@ const express = require('express')
 const route = express.Router();
 const {homepage, message} = require('../controllers/chat.controller')
 
+route.get("/checkstatus", (req, res, next) => {
+    return res.status(200).json({
+        "status": "success",
+        "message": "api ok"
+    })
+})
+
 route.get("/", homepage)
 route.get("/api/message", message)
 
